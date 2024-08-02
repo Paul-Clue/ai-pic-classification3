00d0d6b31abc3780239f636502bb69d48d976a2e
Paul Clue <67766160+Paul-Clue@users.noreply.github.com>
2024-08-02 10:32:35 -0500

    Remove comment

--- src/components/firebase.tsx
+++ src/components/firebase.tsx
@@ -6,11 +6,6 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 // import { firebaseConfig } from './FbCredentials';
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
